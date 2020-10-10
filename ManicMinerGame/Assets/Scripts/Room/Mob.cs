55c3aaaebd8b9c404e094ffd9358d13de635902f
Alexey Shilin <shilin.alexey@gmail.com>
2020-10-10 17:59:27 +0300

    Chg: part 9b - mob refactoring (main)

--- ManicMinerGame/Assets/Scripts/Room/Mob.cs
+++ ManicMinerGame/Assets/Scripts/Room/Mob.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 //namespace Assets.Scripts.Room
 //{
-public class Mob : MovableObject
+public class Mob
 {
+    // private
+    int startFrame;
+    int startX;
+    int startY;
+
+    // public
+    public byte Attribute { get; set; }
+    public int Left { get; set; }
+    public int Right { get; set; }
+    public int Frame { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public int FrameDirection { get; set; }
+
     public List<byte[]> Frames { get; private set; }
 
     public Mob(List<byte[]> frames, int startX, int startY, int left, int right, int startFrame, byte attr)
